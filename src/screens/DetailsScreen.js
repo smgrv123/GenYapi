5e92694e885cfc5d49fd774b741126588a0e628e
Kartikey Mahawar <kartikeymahawar1234@gmail.com>
2021-02-05 11:43:35 +0530

    UI changes

--- src/screens/DetailsScreen.js
+++ src/screens/DetailsScreen.js
@@ -6,31 +6,9 @@ import axios from 'axios';
 
 export default DetailsScreen = ({ route, navigation }) => {
 
-  function deleteconfirmation(uid) {
-    Alert.alert(
-      "Confirm",
-      "Are u sure u want to delete this item?",
-      [
-        {
-          text: "Cancel",
-          onPress: () => console.log("Cancel Pressed"),
-          style: "cancel"
-        },
-        { text: "OK", onPress: () => apidelete(uid) }
-      ],
-      { cancelable: false }
-    );
-  }
+ 
 
-  function apidelete(uid) {
-    axios.delete("https://app-api-geny.herokuapp.com/products/delete",
-      { data: { uuid: uid } })
-      .then(res1 => {
-        console.log(res1.data);
-        console.log({ uid });
-        navigation.replace("Home")
-      })
-  }
+ 
 
   const { name, quantity, uid } = route.params;
 
@@ -43,24 +21,17 @@ export default DetailsScreen = ({ route, navigation }) => {
         padding: 20
       }}>
       <Text
-        style={styles.textst}>
+        style={{ fontSize:50,
+          justifyContent: 'center'
+}}>
         Name: {(name)}
       </Text>
       <Text
-        style={styles.textst}>
+        style={{ fontSize:50,
+          justifyContent: 'center'}}>
         Quantity: {JSON.stringify(quantity)}
       </Text>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => deleteconfirmation(uid)
-        }
-      >
-        <Text
-          style={styles.buttontext}
-        >
-          Delete
-        </Text>
-      </TouchableOpacity>
+     
       <TouchableOpacity
         style={styles.button}
         onPress={() => {
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TouchableOpacity, FlatList, TextInput, Image, ActivityIndicator } from 'react-native';
+import { ConstraintLayout, ConstraintGuide } from "react-constraint-layout";
+import { View, Text, TouchableOpacity, FlatList, TextInput, Image, ActivityIndicator, ScrollView, Alert } from 'react-native';
 import { styles } from "../styles/styles";
 import Axios from 'axios'
 import axios from 'axios';
@@ -7,7 +8,31 @@ import axios from 'axios';
 export default function HomeScreen({ navigation }) {
 
   const [state, setState] = useState();
-  const[loading,setLoading]=useState(true);
+  const [loading, setLoading] = useState(true);
+  function deleteconfirmation(uid) {
+    Alert.alert(
+      "Confirm",
+      "Are u sure u want to delete this item?",
+      [
+        {
+          text: "Cancel",
+          onPress: () => console.log("Cancel Pressed"),
+          style: "cancel"
+        },
+        { text: "OK", onPress: () => apidelete(uid) }
+      ],
+      { cancelable: false }
+    );
+  }
+  function apidelete(uid) {
+    axios.delete("https://app-api-geny.herokuapp.com/products/delete",
+      { data: { uuid: uid } })
+      .then(res1 => {
+        console.log(res1.data);
+        console.log({ uid });
+        navigation.replace("Home")
+      })
+  }
 
   useEffect(() => {
     Axios.get('https://app-api-geny.herokuapp.com/products/all/1')
@@ -16,23 +41,59 @@ export default function HomeScreen({ navigation }) {
         setLoading(false)
       })
   }, [])
-if(loading){
-  return(
-    <View>
-      <ActivityIndicator
-      animating={loading}
-      size={70}
-      color='dimgrey'
-      style={{
-        margin:"50%"
-      }}
-      />
-    </View>
-  )
-}
+  if (loading) {
+    return (
+      <View>
+        <Image
+        source={require("../images/loading_picture.png")}
+        style={{margin:80,
+          marginRight:50,
+          marginTop:150,
+          marginLeft:60,
+        height:350,
+        width:300}}/>
+      
+      </View>
+    )
+  }
   return (
+
+
     <View>
-      <TouchableOpacity style={styles.btn}
+     
+      <FlatList
+        keyExtractor={item => item.uuid}
+        data={state}
+        renderItem={({ item }) => (
+          <View style={styles.container}>
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.plus}
+                onPress={() => navigation.navigate('Details',
+                  {
+                    name: item.name,
+                    quantity: item.quantity,
+                    uid: item.uuid
+                  }
+                )}>
+                {item.name}
+              </Text>
+              <TouchableOpacity style={styles.delbtn}
+              onPress={() => deleteconfirmation(item.uuid)}
+            >
+              <Image
+                source={require("../images/del_image.png")}
+                style={{
+                  resizeMode: 'center',
+                  width: 80,
+                  height: 70,
+                }} />
+            </TouchableOpacity>
+            </TouchableOpacity>
+            
+          </View>
+        )}
+      />
+    <TouchableOpacity style={styles.btn}
         onPress={() => navigation.navigate('Name')}
       >
         <Image
@@ -44,26 +105,7 @@ if(loading){
             borderRadius: 50
           }} />
       </TouchableOpacity>
-      <FlatList
-        keyExtractor={item => item.uuid}
-        data={state}
-        renderItem={({ item }) => (
-          <View>
-            <TouchableOpacity style={styles.button}>
-              <Text style={styles.plus}
-                onPress={() => navigation.push('Details',
-                  {
-                    name: item.name,
-                    quantity: item.quantity,
-                    uid: item.uuid
-                  }
-                )}>
-                {item.name}
-              </Text>
-            </TouchableOpacity>
-          </View>
-        )}
-      />
     </View>
+
   )
 }
